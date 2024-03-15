c3db4a34ed9672d6a34ed07f0f3cb544a08a9f38
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-03-15 23:02:23 +0300

    configured Docker

--- config/settings.py
+++ config/settings.py
@@ -77,15 +77,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# Database
+# Database for DOCKER usage
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
@@ -146,9 +146,9 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-# Celery settings
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL_LOCAL')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND_LOCAL')
+#  Celery Docker settings
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 
 CELERY_TIMEZONE = "Europe/Moscow"
 
