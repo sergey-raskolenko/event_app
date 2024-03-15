5492c8921a121474d81dacce248ac63fa6b10cd3
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-03-15 19:31:46 +0300

    added views

---
+++ myapp/views.py
@@ -0,0 +1,33 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, filters
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
+from .models import Organization, Event
+from .serializers import OrganizationSerializer, EventSerializer, EventFilterSerializer
+
+
+class OrganizationCreateView(generics.CreateAPIView):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+
+
+class EventCreateView(generics.CreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class EventWithUsersListView(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class EventFilteredListView(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventFilterSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    search_fields = ['title']
+    filterset_fields = ['date']
+    ordering_fields = ['date']
+    pagination_class = LimitOffsetPagination
